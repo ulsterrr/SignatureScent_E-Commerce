eecac8c0660543debaf24c5a2ef46d2bf423b048
ulsterrr <notthinginmyhand@gmail.com>
2023-07-18 07:41:18 +0700

    update phần ngàn charts

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -417,6 +417,14 @@ function drawChartDashedLine(data) {
             categories: ['Tháng 1', 'Tháng 2', 'Tháng 3', 'Tháng 4', 'Tháng 5', 'Tháng 6', 'Tháng 7', 'Tháng 8', 'Tháng 9',
                     'Tháng 10', 'Tháng 11', 'Tháng 12']
         },
+        yaxis: {
+                labels: {
+                    formatter: function (val) {
+                        // Format số biểu đồ dạng 1,000,000
+                        return val.toLocaleString() + ' VNĐ';
+                    }
+                }
+            },
         tooltip: {
             y: [{
                 title: {
